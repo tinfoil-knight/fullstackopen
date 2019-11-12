87513a3769deffa5add623a9542dac0ae6dd3cf6
Kunal K <kunal99kundu@gmail.com>
2019-11-12 19:07:20 +0530

    course information step 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,10 +10,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-    <p>{props.part} {props.exercises}</p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </>
   )
 }
@@ -37,10 +47,8 @@ const App = () => {
   return (
     <>
     <Header course={course} />
-    <Content part={part1} exercises={exercises1}/>
-    <Content part={part2} exercises={exercises2}/>
-    <Content part={part3} exercises={exercises3}/>
-    <Total total={exercises1 + exercises2 + exercises3}/>
+    <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+    <Total total={exercises1 + exercises2 + exercises3} />
     </>
   )
 }
