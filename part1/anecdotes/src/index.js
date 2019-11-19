be8a1d44ea3b87697911f131df4fc0f50cc71eb5
Kunal K <kunal99kundu@gmail.com>
2019-11-20 01:08:15 +0530

    anecdotes step 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,33 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Max = (props) => {
+    const max = Math.max(...props.votes)
+    let index = props.votes.indexOf(max)
+
+    if(max===0){
+        return (
+            <p>no votes yet</p>
+        )
+    }
+    return (
+      <>
+      <h1>Anecdote with most votes</h1>
+        <p>{props.anecdotes[index]}</p>
+      </>
+    )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVote] = useState(new Array(6).fill(0))
+
   const upVote = (selected) => {
     const copy = [...votes]
     copy[selected] += 1
     setVote(copy)
   }
 
-
-  console.log(selected)
-  console.log(votes)
-
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
+    <h1>Anecdote of the day</h1>
+        <p>{props.anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
       <p>
-      <button onClick={() => upVote(selected)}>vote</button>
-      <button onClick={() => setSelected(Math.floor(Math.random() * (6)))}>next anecdote</button>
+        <button onClick={() => upVote(selected)}>vote</button>
+        <button onClick={() => setSelected(Math.floor(Math.random() * (6)))}>next anecdote</button>
       </p>
+    <Max votes = {votes} anecdotes={props.anecdotes} />
     </div>
   )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
+  'If it hurts, do it more often.',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
