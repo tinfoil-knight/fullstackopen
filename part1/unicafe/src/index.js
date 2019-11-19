4d573538d4901840b51281f083f764234e407753
Kunal K <kunal99kundu@gmail.com>
2019-11-19 19:22:37 +0530

    unicafe step 6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistic = ({text,value}) => {
+
   return (
     <>
-    <p>{text} {value}</p>
+      <td>{text}</td>
+      <td>{value}</td>
     </>
   )
+
 }
 
 const Statistics = ({good, neutral, bad}) => {
@@ -18,32 +21,38 @@ const Statistics = ({good, neutral, bad}) => {
   if (total===0){
     return (
       <>
-      <p>No feedback given</p>
+        <p>No feedback given</p>
       </>
     )
   }
 
   return (
     <>
-    <Statistic text="good" value={good} />
-    <Statistic text="neutral" value={neutral} />
-    <Statistic text="bad" value={bad} />
-    <Statistic text="all" value={total} />
-    <Statistic text="average" value={average} />
-    <Statistic text="positive" value={positive+" %"} />
+    <table>
+      <tbody>
+      <tr><Statistic text="good" value={good} /></tr>
+      <tr><Statistic text="neutral" value={neutral} /></tr>
+      <tr><Statistic text="bad" value={bad} /></tr>
+      <tr><Statistic text="all" value={total} /></tr>
+      <tr><Statistic text="average" value={average} /></tr>
+      <tr><Statistic text="positive" value={positive+" %"} /></tr>
+      </tbody>
+    </table>
     </>
   )
 
 }
 
 const Button = ({good, neutral, bad, ftnGood, ftnBad, ftnNeutral}) => {
+
   return (
     <>
-    <button onClick={ftnGood}>good</button>
-    <button onClick={ftnNeutral}>neutral</button>
-    <button onClick={ftnBad}>bad</button>
+      <button onClick={ftnGood}>good</button>
+      <button onClick={ftnNeutral}>neutral</button>
+      <button onClick={ftnBad}>bad</button>
     </>
   )
+
 }
 
 const App = () => {
@@ -54,12 +63,13 @@ const App = () => {
 
   return (
     <>
-    <h1>give feedback</h1>
-      <Button ftnGood={() => setGood(good+1)} ftnNeutral={() => setNeutral(neutral+1)} ftnBad={() => setBad(bad+1)} />
-    <h1>statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <h1>give feedback</h1>
+        <Button ftnGood={() => setGood(good+1)} ftnNeutral={() => setNeutral(neutral+1)} ftnBad={() => setBad(bad+1)} />
+      <h1>statistics</h1>
+        <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   )
+
 }
 
 ReactDOM.render(<App />,
