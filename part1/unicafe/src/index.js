aa68c090215d8cb49ec47f96f5632ee29d5e3e68
Kunal K <kunal99kundu@gmail.com>
2019-11-19 19:12:13 +0530

    unicafe step 5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-  const good = props.good
-  const neutral = props.neutral
-  const bad = props.bad
+const Statistic = ({text,value}) => {
+  return (
+    <>
+    <p>{text} {value}</p>
+    </>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+
   const total = good + neutral + bad
-  if (total==0){
+  const average = (good*1 + neutral*0 + bad*(-1))/(total)
+  const positive = good*100/(total)
+
+  if (total===0){
     return (
       <>
       <p>No feedback given</p>
@@ -16,16 +25,27 @@ const Statistics = (props) => {
 
   return (
     <>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {total}</p>
-    <p>average {(good*1 + neutral*0 + bad*(-1))/(total)}</p>
-    <p>positive {good*100/(total)} %</p>
+    <Statistic text="good" value={good} />
+    <Statistic text="neutral" value={neutral} />
+    <Statistic text="bad" value={bad} />
+    <Statistic text="all" value={total} />
+    <Statistic text="average" value={average} />
+    <Statistic text="positive" value={positive+" %"} />
     </>
   )
 
 }
+
+const Button = ({good, neutral, bad, ftnGood, ftnBad, ftnNeutral}) => {
+  return (
+    <>
+    <button onClick={ftnGood}>good</button>
+    <button onClick={ftnNeutral}>neutral</button>
+    <button onClick={ftnBad}>bad</button>
+    </>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
@@ -35,11 +55,9 @@ const App = () => {
   return (
     <>
     <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <Button ftnGood={() => setGood(good+1)} ftnNeutral={() => setNeutral(neutral+1)} ftnBad={() => setBad(bad+1)} />
     <h1>statistics</h1>
-    <Statistics good={good} bad={bad} neutral={neutral} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   )
 }
