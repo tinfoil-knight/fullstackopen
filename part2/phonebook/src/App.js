21635fb278c57a9112f009528a8117fcef1b5dde
Kunal K <kunal99kundu@gmail.com>
2019-12-05 11:47:38 +0530

    phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,36 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([])
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  // const [ persons, setPersons] = useState([])
+
+  // state handlers
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newQuery, setNewQuery ] = useState('')
 
-  // Event Handlers
+  //event handler for search query
+  const handleQueryChange = (event) =>
+  {
+    setNewQuery(event.target.value)
+  }
 
-  // for form
+  // function for filtering using indexOf
+  const filterItems = (array, query) => {
+    return array.filter(el => el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  // The indexOf() method returns the position of the first occurrence of a specified value in a string.
+  // This method returns -1 if the value to search for never occurs.
+  // Note: The indexOf() method is case sensitive.
+
+
+  // event handler for form
   const addContact = (event) =>
   {
     event.preventDefault()
@@ -16,7 +39,7 @@ const App = () => {
     const checker = persons.filter(person => person.name===newName);
     if (newName){
       if (checker.length!==0){
-        // notice how a variable and string are joined here using a template literals
+        // notice how a variable and string are joined here using a template literal
         // NB: `` (backtick) are used, not "" or '' (quotes)
         window.alert(`${newName} is already added to phonebook`)
         // alt: newName + "is already added to phonebook"
@@ -31,24 +54,39 @@ const App = () => {
         setNewNumber("")
       }
     }
-
-
   }
 
-  // for input
+  // event handler for input fields
   const handleNameChange = (event) =>
   {
     setNewName(event.target.value)
   }
-
   const handleNumberChange = (event) =>
   {
     setNewNumber(event.target.value)
   }
 
+  // array selector for display
+  const namesToShow = newQuery
+    ? filterItems(persons, newQuery)
+    : persons
+
+  // display component (factor this out)
+  const rows = () => {
+    return (
+      namesToShow.map(el => <p key={el.name}>{el.name} {el.number}</p>)
+    )
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        <div>
+          <div>filter shown with <input value={newQuery} onChange={handleQueryChange}/></div>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addContact}>
         <div>
           <div>name: <input value={newName} onChange={handleNameChange} /></div>
@@ -59,11 +97,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {
-        persons.map(person =>
-          <p key={person.name}>{person.name} {person.number}</p>
-        )
-      }
+      {rows()}
     </div>
   )
 
