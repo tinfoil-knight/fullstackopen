535c2bbb2536c069fdd06a917a06cf219dd2a8b3
Kunal K <kunal99kundu@gmail.com>
2019-12-13 01:29:43 +0530

    phonebook backend step 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -32,5 +32,16 @@ app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people` + "<br>" + `${new Date()}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, ()=>{console.log(`Server running on ${PORT}`)})
