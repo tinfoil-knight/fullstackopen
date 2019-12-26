a8dbc5fcc32985bbebdd7b9d32d2cfaca6ae3b3c
tinfoil-knight <kunal99kundu@gmail.com>
2019-12-26 16:23:47 +0530

    helper functions and unit tests step 2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (array) => {
+
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
