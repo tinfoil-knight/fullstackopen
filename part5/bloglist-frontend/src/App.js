5b029a1fcf4ca81271533c101e436891e8322d8d
tinfoil-knight <kunal99kundu@gmail.com>
2020-02-20 00:32:23 +0530

    bloglist frontend step 2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -18,6 +18,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      fetchService.setToken(user.token)
+    }
+  }, [])
+
   
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -25,12 +34,20 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password })
 
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       alert(exception)
     }
+  }
+
+  const handleLogout = (event) => {
+    window.localStorage.clear()
   }
 
   if (user === null) {
@@ -55,7 +72,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      {user.username} logged in
+      {user.name} logged in <button type="button" onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
