7a4f026800dff982673ce2a3136b2cc02d7d51ab
tinfoil-knight <kunal99kundu@gmail.com>
2020-03-30 14:46:01 +0530

    bloglist end to end testing step 2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,41 @@
-describe('Blog app', function() {
-    beforeEach(function() {
-      cy.request('POST', 'http://localhost:3003/api/testing/reset')
-      cy.visit('http://localhost:3000')
-    })
-  
-    it('Login from is shown', function() {
-      cy.contains('Log in to application')
-    })
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    cy.visit('http://localhost:3000')
   })
+
+  it('Login from is shown', function () {
+    cy.contains('Log in to application')
+  })
+})
+
+describe('Login', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
+    cy.visit('http://localhost:3000')
+  })
+
+  it('succeeds with correct credentials', function () {
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.contains('login').click()
+    cy.contains('Matti Luukkainen logged in')
+  })
+
+  it('fails with wrong credentials', function () {
+    cy.get('#username').type('jhdfjksh')
+    cy.get('#password').type('aklakala')
+    cy.contains('login').click()
+    cy.contains('wrong username or password')
+  })
+})
--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -98,11 +98,11 @@ const App = () => {
         <form onSubmit={handleLogin}>
           <div>
             username
-            <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+            <input type="text" value={username} id="username" name="Username" onChange={({ target }) => setUsername(target.value)} />
           </div>
           <div>
             password
-            <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+            <input type="password" value={password} id="password" name="Password" onChange={({ target }) => setPassword(target.value)} />
           </div>
           <button type="submit">login</button>
         </form>
