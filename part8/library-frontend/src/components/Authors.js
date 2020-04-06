02197eadb2a2bf906ff1fcd5e28adc0d2ff74f83
tinfoil-knight <kunal99kundu@gmail.com>
2020-04-06 11:40:05 +0530

    Authors view

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS } from '../queries'
+
+
+
+const Authors = (props) => {
+
+  const results = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (results.loading) {
+    return <div>loading...</div>
+  }
+
+  const authors = results.data.allAuthors
+
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
